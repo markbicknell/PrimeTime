ac44a8462d1d64138a2a5bc69caf33ae48605185
Mark Bicknell <markbicknell@gmail.com>
2020-07-12 22:47:46 +0100

    Ubuntu build fix

--- cpp/cpu_generator/cpu_generator.h
+++ cpp/cpu_generator/cpu_generator.h
@@ -1,4 +1,4 @@
 #include <vector>
 
 // Generate a requested number of primes, starting at zero.
-std::vector<std::uint64_t> generate_primes(size_t num_primes_to_generate);
+std::vector<uint64_t> generate_primes(size_t num_primes_to_generate);
